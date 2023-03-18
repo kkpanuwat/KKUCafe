fbdb93cef558ea380149ff874181b0ecfdc9ccc5
kkpanuwat <panuwat20481@gmail.com>
2023-03-18 14:29:18 +0700

    feature:CRUD material

--- controller/MaterialController.php
+++ controller/MaterialController.php
@@ -2,34 +2,34 @@
 session_start();
 require('./database/connect.php');
 
-// add productType
+// add material
 if ($_POST['_method'] === 'POST') {
-    if (empty($_POST['productTypeName'])) {
+    if (empty($_POST['materialName'])) {
         header('Location: ../index.php');
     }
-    $productTypeName = $_POST['productTypeName'];
-    $sql_command = "INSERT INTO productType(productTypeName) values ('$productTypeName')";
+    $materialName = $_POST['materialName'];
+    $sql_command = "INSERT INTO material(materialName) values ('$materialName')";
     mysqli_query($db, $sql_command);
-    header('Location: ../productType.php');
+    header('Location: ../material.php');
 }
 
 if ($_POST['_method'] === 'PUT') {
-    if (empty($_POST['productTypeID']) || empty($_POST['productTypeName'])) {
+    if (empty($_POST['materialID']) || empty($_POST['materialName'])) {
         header('Location: ../index.php');
     }
-    $productTypeID = $_POST['productTypeID'];
-    $productTypeName = $_POST['productTypeName'];
-    $sql_command = "UPDATE productType set productTypeName = '$productTypeName' where productTypeID = $productTypeID";
+    $materialID = $_POST['materialID'];
+    $materialName = $_POST['materialName'];
+    $sql_command = "UPDATE material set materialName = '$materialName' where materialID = $materialID";
     mysqli_query($db, $sql_command);
-    header('Location: ../productType.php');
+    header('Location: ../material.php');
 }
 
 if ($_POST['_method'] === 'DELETE') {
-    if (empty($_POST['productTypeID'])) {
+    if (empty($_POST['materialID'])) {
         header('Location: ../index.php');
     }
-    $productTypeID = $_POST['productTypeID'];
-    $sql_command = "DELETE FROM productType where productTypeID = $productTypeID";
+    $materialID = $_POST['materialID'];
+    $sql_command = "DELETE FROM material where materialID = $materialID";
     mysqli_query($db, $sql_command);
-    header('Location: ../productType.php');
+    header('Location: ../material.php');
 }
--- material.php
+++ material.php
@@ -14,14 +14,14 @@ require('./common/header.php');
                     <div class="btn-add-unit clearfix js-item-menu">
                         <button class="btn btn-primary">เพิ่มประเภทวัตถุดิบ</button>
                         <div class="account-dropdown js-dropdown">
-                            <form action="./controller/ProductTypeController.php" method="post">
+                            <form action="./controller/MaterialController.php" method="post">
                                 <div class="add-unit-blog">
                                     <div class="text-title">
                                         ชื่อวัตถุดิบที่ต้องการเพิ่ม
                                     </div>
                                     <hr />
                                     <input type="hidden" class="form-control" name="_method" value='POST' />
-                                    <input type="text" class="form-control" name="productTypeName" placeholder="กาแฟ" required />
+                                    <input type="text" class="form-control" name="materialName" placeholder="กาแฟ" required />
                                     <div class="row mt-3">
                                         <div class="col-6 d-flex justify-content-end">
                                             <button type="reset" class="btn btn-danger btn-add-unit-dropdown">ลบข้อมูล</button>
@@ -63,23 +63,23 @@ require('./common/header.php');
                             ?>
                                 <tr>
                                     <td><?= $index ?></td>
-                                    <td><?= $row['productTypeName'] ?></td>
+                                    <td><?= $row['materialName'] ?></td>
                                     <td> <!-- Modal -->
-                                        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#exampleModalCenter<?= $row['productTypeID'] ?>">
+                                        <button type="button" class="btn btn-warning" data-toggle="modal" data-target="#exampleModalCenter<?= $row['materialID'] ?>">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                                                 <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z" />
                                                 <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z" />
                                             </svg>
                                         </button>
-                                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModalCenterDelete<?= $row['productTypeID'] ?>">
+                                        <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModalCenterDelete<?= $row['materialID'] ?>">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3-fill" viewBox="0 0 16 16">
                                                 <path d="M11 1.5v1h3.5a.5.5 0 0 1 0 1h-.538l-.853 10.66A2 2 0 0 1 11.115 16h-6.23a2 2 0 0 1-1.994-1.84L2.038 3.5H1.5a.5.5 0 0 1 0-1H5v-1A1.5 1.5 0 0 1 6.5 0h3A1.5 1.5 0 0 1 11 1.5Zm-5 0v1h4v-1a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5ZM4.5 5.029l.5 8.5a.5.5 0 1 0 .998-.06l-.5-8.5a.5.5 0 1 0-.998.06Zm6.53-.528a.5.5 0 0 0-.528.47l-.5 8.5a.5.5 0 0 0 .998.058l.5-8.5a.5.5 0 0 0-.47-.528ZM8 4.5a.5.5 0 0 0-.5.5v8.5a.5.5 0 0 0 1 0V5a.5.5 0 0 0-.5-.5Z" />
                                             </svg>
                                         </button>
                                         <!-- edit modal -->
-                                        <div class="modal fade" id="exampleModalCenter<?= $row['productTypeID'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                                            <form action="./controller/ProductTypeController.php" method="post">
-                                                <input type="hidden" name="productTypeID" value="<?= $row['productTypeID'] ?>">
+                                        <div class="modal fade" id="exampleModalCenter<?= $row['materialID'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                            <form action="./controller/MaterialController.php" method="post">
+                                                <input type="hidden" name="materialID" value="<?= $row['materialID'] ?>">
                                                 <input type="hidden" name="_method" value="PUT">
                                                 <div class="modal-dialog modal-dialog-centered" role="document">
                                                     <div class="modal-content">
@@ -90,7 +90,7 @@ require('./common/header.php');
                                                             </button>
                                                         </div>
                                                         <div class="modal-body">
-                                                            <input type="text" class="form-control" name="productTypeName" placeholder="ml:มิลลิกรัม" value="<?= $row['productTypeName'] ?>" required />
+                                                            <input type="text" class="form-control" name="materialName" placeholder="ml:มิลลิกรัม" value="<?= $row['materialName'] ?>" required />
                                                         </div>
                                                         <div class="modal-footer">
                                                             <button type="reset" class="btn btn-warning" data-dismiss="modal">ลบข้อมูล</button>
@@ -101,10 +101,10 @@ require('./common/header.php');
                                             </form>
                                         </div>
                                         <!-- delete modal -->
-                                        <div class="modal fade" id="exampleModalCenterDelete<?= $row['productTypeID'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
-                                            <form action="./controller/ProductTypeController.php" method="post">
+                                        <div class="modal fade" id="exampleModalCenterDelete<?= $row['materialID'] ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+                                            <form action="./controller/MaterialController.php" method="post">
                                                 <input type="hidden" name="_method" value="DELETE">
-                                                <input type="hidden" name="productTypeID" value="<?= $row['productTypeID'] ?>">
+                                                <input type="hidden" name="materialID" value="<?= $row['materialID'] ?>">
                                                 <div class="modal-dialog modal-dialog-centered" role="document">
                                                     <div class="modal-content">
                                                         <div class="modal-header">
@@ -114,7 +114,7 @@ require('./common/header.php');
                                                             </button>
                                                         </div>
                                                         <div class="modal-body">
-                                                            <input type="text" class="form-control" name="productTypeName" placeholder="ml:มิลลิกรัม" value="<?= $row['productTypeName'] ?>" required readonly />
+                                                            <input type="text" class="form-control" name="materialName" placeholder="ml:มิลลิกรัม" value="<?= $row['materialName'] ?>" required readonly />
                                                         </div>
                                                         <div class="modal-footer">
                                                             <button type="button" class="btn btn-info" data-dismiss="modal">ปิดหน้าต่าง</button>
